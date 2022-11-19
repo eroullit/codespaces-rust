1036694c5b890ce92b7198867d9d2d11574777d8
Emmanuel Roullit <eroullit@github.com>
2022-11-19 21:56:05 +0000

    add basic rust tcp server based on artix
    Signed-off-by: Emmanuel Roullit <eroullit@github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(fs::Files::new("/static", "./static/").show_files_listing())
+        .service(fs::Files::new("/", "./templates/").index_file("index.html"))
+    })
+    .bind(("127.0.0.1", 8888))?
+    .run()
+    .await
 }
